699bcfd71f40d89d67a44daf70933e3e11457c88
Victor <issuehung@gmail.com>
2017-01-15 18:22:41 +0800

    first commit

---
+++ counttheOdd.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+//method 1.
+
+var b = 0
+
+for i in  1...100{
+    if i % 2 == 0{
+    }else{
+        b = b + i
+        print(b)
+    }
+
+}
+
+// method 2.
+print(pow(((99-1)/2)+1,2))
+
+
